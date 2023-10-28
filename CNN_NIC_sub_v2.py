fe7f26da233a17b0e052479a8cfa092096239bd3
zihaofoo <zihaofoo@mit.edu>
2023-10-28 17:34:45 -0400

    10/28 ZHf

--- CNN_NIC_sub_v2.py
+++ CNN_NIC_sub_v2.py
@@ -241,7 +241,7 @@ def create_combined_model(num_conv_layers=3, conv_layer_size=171, num_dense_laye
     
     # Add convolutional layers dynamically
     for _ in range(num_conv_layers):
-        x = Conv2D(conv_layer_size, (3, 3), activation='relu', padding='same', kernel_regularizer=l2(0.01))(x)
+        x = Conv2D(conv_layer_size, (3, 3), activation='relu', padding='same')(x)
         
     x = Flatten()(x)
     x = Dense(dense_layer_size, activation='relu')(x)
@@ -260,7 +260,7 @@ def create_combined_model(num_conv_layers=3, conv_layer_size=171, num_dense_laye
     
     # Add dense layers dynamically
     for _ in range(num_dense_layers):
-        y = Dense(dense_layer_size, activation='relu', kernel_regularizer=l2(0.01))(y)
+        y = Dense(dense_layer_size, activation='relu')(y)
         
     dense_branch = Dense(dense_layer_size, activation='relu')(y)
     
