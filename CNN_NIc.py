1dea0ae552f6b3b5ef14a326a12627a64e74b805
zihaofoo <zihaofoo@mit.edu>
2023-11-01 15:00:17 -0400

    11/1 ZHF Push

--- CNN_NIc.py
+++ CNN_NIc.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from CNN_NIC_sub_v2 import *
 from scipy.ndimage import rotate
 from tensorflow.keras.models import load_model
+import pdb
 
 seed_number = 42
 np.random.seed(seed_number)
@@ -16,17 +17,17 @@ ratings = np.load("datasets/scores.npy") # Load advisor scores
 score_order = ["Wellness", "Tax", "Transportation", "Business"] #This is the order of the scores in the dataset
 ratings_df = pd.DataFrame(ratings, columns = score_order) #Create a dataframe
 
-# model0 = get_trained_model(advisor_val = 0, eval_mode = True, seed_number = seed_number)
-# model0.save("model0.h5")
-# 
-# model1 = get_trained_model(advisor_val = 1, eval_mode = True, seed_number = seed_number)
-# model1.save("model1.h5")
-# 
-# model2 = get_trained_model(advisor_val = 2, eval_mode = True, seed_number = seed_number)
-# model2.save("model2.h5")
-# 
-# model3 = get_trained_model(advisor_val = 3, eval_mode = True, seed_number = seed_number)
-# model3.save("model3.h5")
+model0 = get_trained_model(advisor_val = 0, eval_mode = True, seed_number = seed_number)
+model0.save("model0.h5")
+
+model1 = get_trained_model(advisor_val = 1, eval_mode = True, seed_number = seed_number)
+model1.save("model1.h5")
+
+model2 = get_trained_model(advisor_val = 2, eval_mode = True, seed_number = seed_number)
+model2.save("model2.h5")
+
+model3 = get_trained_model(advisor_val = 3, eval_mode = True, seed_number = seed_number)
+model3.save("model3.h5")
 
 # Load the '.npz' file
 # loaded_data = np.load('grids_advisor2_good.npz')
@@ -37,46 +38,47 @@ features0 = []
 features1 = []
 features2 = []
 features3 = []
+# 
+for grid in grids:
+    features = compute_features(grid, advisor = 0)
+    features = np.nan_to_num(features, nan = 0)
+    features0.append(features)
+features0 = np.array(features0)
+features0[np.isnan(features0)] = 0
+features0 = features0.astype(np.float64)
+np.save('features0.npy', features0)
 
-## for grid in grids:
-##     features = compute_features(grid, advisor = 0)
-##     features = np.nan_to_num(features, nan = 0)
-##     features0.append(features)
-## 
-##     features = compute_features(grid, advisor = 1)
-##     features = np.nan_to_num(features, nan = 0)
-##     features1.append(features)
-## 
-##     features = compute_features(grid, advisor = 2)
-##     features = np.nan_to_num(features, nan = 0)
-##     features2.append(features)
-## 
-##     features = compute_features(grid, advisor = 3)
-##     features = np.nan_to_num(features, nan = 0)
-##     features3.append(features)
-## 
-## features0 = np.array(features0)
-## features0[np.isnan(features0)] = 0
-## features1 = np.array(features1)
-## features1[np.isnan(features1)] = 0
-## features2 = np.array(features2)
-## features2[np.isnan(features2)] = 0
-## features3 = np.array(features3)
-## features3[np.isnan(features3)] = 0   
-## 
-## 
-## grids_onehot = np.array([one_hot_encode(grid) for grid in grids])
-## grids_onehot = grids_onehot.astype(np.float64)
-## features0 = features0.astype(np.float64)
-## features1 = features1.astype(np.float64)
-## features2 = features2.astype(np.float64)
-## features3 = features3.astype(np.float64)
-## 
-# np.save('features0.npy', features0)
-# np.save('features1.npy', features1)
-# np.save('features2.npy', features2)
-# np.save('features3.npy', features3)
-# np.savez('grids_onehot.npz', grids_onehot)
+for grid in grids:
+    features = compute_features(grid, advisor = 1)
+    features = np.nan_to_num(features, nan = 0)
+    features1.append(features)
+features1 = np.array(features1)
+features1[np.isnan(features1)] = 0
+features1 = features1.astype(np.float64)
+np.save('features1.npy', features1)
+
+for grid in grids:
+    features = compute_features(grid, advisor = 2)
+    features = np.nan_to_num(features, nan = 0)
+    features2.append(features)
+features2 = np.array(features2)
+features2[np.isnan(features2)] = 0
+features2 = features2.astype(np.float64)
+np.save('features2.npy', features2)
+
+for grid in grids:
+    features = compute_features(grid, advisor = 3)
+    features = np.nan_to_num(features, nan = 0)
+    features3.append(features)
+features3 = np.array(features3)
+features3[np.isnan(features3)] = 0   
+features3 = features3.astype(np.float64)
+np.save('features3.npy', features3)
+
+
+grids_onehot = np.array([one_hot_encode(grid) for grid in grids])
+grids_onehot = grids_onehot.astype(np.float64)
+np.savez('grids_onehot.npz', grids_onehot)
 
 features0 = np.load('features0.npy')
 features1 = np.load('features1.npy')
